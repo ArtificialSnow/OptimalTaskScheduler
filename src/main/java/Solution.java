19b8fbe7adf1ec8431dfd561fe72a490ebfeb1f0
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2020-08-28 17:26:22 +1200

    Refactored solution classes
    Changed solution back to abstract class
    Moved fields to solution
    Removed FTO from parallel solution
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,32 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 
-public interface Solution {
+public abstract class Solution {
+    protected TaskGraph taskGraph;
+    protected int numProcessors;
+    protected int numTasks;
 
-    Schedule run(TaskGraph taskGraph, int numProcessors, int upperBoundTime) throws IOException, ClassNotFoundException;
+    protected int[] nodePriorities;
+    protected ArrayList<Integer>[] equivalentNodesList;
+    protected int[] maxLengthToExitNode;
+
+    protected volatile int[] bestStartTime; // bestStartTime[i] => start time of task i in best schedule found so far
+    protected volatile int[] bestScheduledOn; // bestScheduledOn[i] => processor that task i is scheduled on, in best schedule
+    protected volatile int bestFinishTime; // earliest finishing time of schedules we have searched
+    protected volatile HashSet<Integer> seenSchedules = new HashSet<>();
+
+    public abstract Schedule run(TaskGraph taskGraph, int numProcessors, int upperBoundTime) throws IOException, ClassNotFoundException;
+
+    protected void updateTasksScheduled(int tasksScheduled) {
+        // DO NOTHING
+    }
+
+    protected void updateCurrentBest(int currentBest) {
+        // DO NOTHING
+    }
+
+    protected void updateStateCount(int stateCount) {
+        // DO NOTHING
+    }
 }
